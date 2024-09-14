73a56da792a2c9221cb5ca3ee1b41c4d0f7810ef
Adil Kamran <adilkamranadilkamran056@gmail.com>
2024-09-15 03:39:05 +0500

    Add files via upload

---
+++ New folder/index.js
@@ -0,0 +1,6 @@
+var green = Math.round(Math.random() * 275);
+var red = Math.round(Math.random() * 275);
+var blue = Math.round(Math.random() * 275);
+
+document.getElementById("color").style.backgroundColor = `rgb(${green}, ${red}, ${blue})`;
+
